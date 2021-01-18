bf8c0fd617b7947ea6132541d3131d7c5ecb6344
Diego Marcelino <diego@smartbr.com>
2021-01-18 08:47:33 -0300

    botão formulário

--- src/components/register-form/styles.ts
+++ src/components/register-form/styles.ts
@@ -12,18 +12,21 @@ export const Title = styled.h2`
   font-size: 2.5rem;
   margin: 10px 0;
   text-align: center;
-  color: #64dd17;
+  color: #00cc39;
 `;
 
 export const Button = styled.button`
-  background: #64dd17;
+  background: #00cc39;
   border: none;
   outline: none;
   border-radius: 5px;
   padding: 8px 12px;
-  font-size: 15px;
+  font-size: 18px;
   margin: 20px 0 0 0;
   width: 100%;
+  height: 45px;
+  font-weight: bold;
+  cursor: pointer;
 `;
 
 export const Phone = styled.div`
